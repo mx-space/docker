678ced3bb2c5df179566740d553b74258e6f25fc
Innei <tukon479@gmail.com>
2021-10-02 18:31:55 +0800

    feat: build script

---
+++ build.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env zx
+
+const { readFileSync, writeFileSync } = require('fs')
+const { $, question } = require('zx')
+
+async function main() {
+  const domain = await question('Your domain name is: ')
+  const email = await question('Your email is: ')
+
+  const caddy = readFileSync('./Caddyfile', { encoding: 'utf8' })
+
+  writeFileSync(
+    './Caddyfile',
+    caddy
+      .replace(/dev\.shizuri\.net/g, domain)
+      .replace('tukon@gmail.com', email),
+    {
+      encoding: 'utf-8',
+    },
+  )
+
+  await $`git clone https://github.com/mx-space/kami --depth=1`
+  await $`docker-compose build`
+  await $`docker-compose up -d`
+}
+
+main()
